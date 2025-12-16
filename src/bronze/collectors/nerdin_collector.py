4995555c372681db992a75e648723a1b5208f81e
VictorClvtt <joaoclivatti@hotmail.com>
2025-12-16 17:28:59 -0300

    feat: add functions to scrape vagas.com and nerdin jobs pages

---
+++ src/bronze/collectors/nerdin_collector.py
@@ -0,0 +1,30 @@
+from src.utils.web_scraping import collect_pages
+from src.utils.bucket import file_to_bucket
+
+def nerdin_collector(
+    bucket_endpoint,
+    access_key,
+    secret_key,
+    bucket_name,
+    today_str,
+    max_pages,
+    base_url,
+    headers
+):
+    print("[INFO] Starting Nerdin Bronze ingestion\n")
+
+    file_to_bucket(
+        bucket_endpoint=bucket_endpoint,
+        access_key=access_key,
+        secret_key=secret_key,
+        bucket_name=bucket_name,
+        path=f"bronze/JSON/nerdin/nerdin-pages-{today_str}.json",
+
+        data=collect_pages(
+            max_pages=max_pages,
+            base_url=base_url,
+            headers=headers
+        )
+    )
+
+    print("\n[INFO] Nerdin Bronze ingestion finished successfully")
--- src/bronze/run_bronze.py
+++ src/bronze/run_bronze.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 from src.utils.variables import load_env_vars
+
 from src.bronze.collectors.programathor_collector import programathor_collector
+from src.bronze.collectors.vagascom_collector import vagascom_collector
+from src.bronze.collectors.nerdin_collector import nerdin_collector
 
 MAX_PAGES = 5
 
 PROGRAMATHOR_URL = "https://programathor.com.br/jobs/page/"
 VAGASCOM_URL = "https://www.vagas.com.br/vagas-de-tecnologia?&pagina="
+NERDIN_URL = "https://www.nerdin.com.br/vagas"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (compatible; DataCollector/1.0)"
@@ -23,6 +27,32 @@ def main():
         today_str=today_str,
         max_pages=MAX_PAGES,
         base_url=PROGRAMATHOR_URL,
+        headers=HEADERS
+    )
+
+    print("\n")
+
+    vagascom_collector(
+        bucket_endpoint=bucket_endpoint,
+        access_key=access_key,
+        secret_key=secret_key,
+        bucket_name=bucket_name,
+        today_str=today_str,
+        max_pages=MAX_PAGES,
+        base_url=VAGASCOM_URL,
+        headers=HEADERS
+    )
+
+    print("\n")
+
+    nerdin_collector(
+        bucket_endpoint=bucket_endpoint,
+        access_key=access_key,
+        secret_key=secret_key,
+        bucket_name=bucket_name,
+        today_str=today_str,
+        max_pages=None,
+        base_url=NERDIN_URL,
         headers=HEADERS
     )
 
