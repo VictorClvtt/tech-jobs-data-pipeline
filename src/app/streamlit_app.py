b020ef9ed86687c0a9338a298dc4c8d51a8a3677
Victor Clivatti <joaoclivatti@hotmail.com>
2026-01-09 01:17:20 -0300

    fix: fix module import so it works both locally and on Streamlit's Cloud

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import duckdb
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parents[2]
+sys.path.append(str(ROOT_DIR))
+
 from src.utils.variables import load_env_vars
 
 # ======================================================
